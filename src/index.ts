0c955159e39a6ae196ad4dec87c84d62811714e7
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-06-13 00:39:20 +0100

    adding a CsvFileReader class

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
 //reading & parsing csv data
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8',
-  })
-  .split('\n')
-  .map((row: string): string[] => row.split(','));
+const reader = new CsvFileReader('football.csv');
+reader.read();
 
-// for (let match of matches) {
-//   if (match[1] === 'Man United' && match[5] === 'H') {
-//     console.log('man won');
-//   }
-// }
+//analyzing data
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+//generating a report
+console.log(`man yunited won ${manUnitedWins} games!`);
