af6218742bdcf098d7b79ee7d75918091a4e5196
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-06-13 00:18:38 +0100

    reading & parsing csv data

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('working');
+import fs from 'fs';
+
+//reading & parsing csv data
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((row: string): string[] => row.split(','));
+
+// for (let match of matches) {
+//   if (match[1] === 'Man United' && match[5] === 'H') {
+//     console.log('man won');
+//   }
+// }
