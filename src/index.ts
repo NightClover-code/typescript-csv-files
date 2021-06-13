da3ddfef9f5dfe108805faf7858438a16d631163
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-06-14 00:57:22 +0100

    understanding composition vs inheritance

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 //importing classes & utils
+import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
 import { MatchResult } from './types';
 
 //reading & parsing csv data
-const reader = new MatchReader('football.csv');
-reader.read();
+const csvFileReader = new CsvFileReader('football.csv');
+
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 //analyzing data
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
