da0b9201492ce416d9f37113da476593260ab638
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-06-21 23:23:44 +0100

    switching to composition

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-//importing classes & utils
-import { MatchReader } from './inheritance/MatchReader';
+// importing classes & utils
+import { MatchReader } from './composition/MatchReader';
+import { Summary } from './composition/Summary';
 
-//reading & parsing csv data
-const matchReader = new MatchReader('football.csv', 'Man United');
-matchReader.execute();
+// reading & parsing csv data
+const matchReader = MatchReader.fromCsv('football.csv');
+matchReader.load();
+
+// analyzing data
+const summary = Summary.winsAnalysisAndHtmlReport('Man United');
+
+// generating a report
+summary.buildAndPrintReport(matchReader.matches);
